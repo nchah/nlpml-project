9fa2a88efa51f6e71af9dafa6eccde813114cf59
Niel C <nielchah@gmail.com>
2016-11-27 17:55:49 -0500

    Sklearn cluster images

--- sklearn/sklearn-cluster.py
+++ sklearn/sklearn-cluster.py
@@ -71,7 +71,7 @@ def plot_scores_and_clusters(X):
     # Plotting silhouette scores and the clusters
     # Adapted from http://scikit-learn.org/stable/auto_examples/cluster/
     # plot_kmeans_silhouette_analysis.html
-    range_n_clusters = [2, 3]
+    range_n_clusters = [2, 3, 4, 5, 6, 7, 8, 9]
     # matplotlib.use('TkAgg')
     for n_clusters in range_n_clusters:
         # Create a subplot with 1 row and 2 columns

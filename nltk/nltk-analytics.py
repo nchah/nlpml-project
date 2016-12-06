63912aad120a15753efa63babba168bd758f6bf3
Niel C <nielchah@gmail.com>
2016-12-06 10:43:20 -0500

    Added csv saving for nltk

--- nltk/nltk-analytics.py
+++ nltk/nltk-analytics.py
@@ -2,8 +2,10 @@
 
 
 from nltk import word_tokenize
+from nltk.collocations import *
 from pprint import pprint
 import argparse
+import csv
 import nltk
 import string
 import pandas as pd
@@ -35,10 +37,28 @@ u'don', u'should', u'now', u'd', u'll', u'm', u'o', u're', u've', u'y', u'ain', 
 u'doesn', u'hadn', u'hasn', u'haven', u'isn', u'ma', u'mightn', u'mustn', u'needn', u'shan', u'shouldn', u'wasn', 
 u'weren', u'won', u'wouldn'] """
 
+# Setting up CSVs
+fd = '' #'nltk/'
+freq_dist_csv = fd + 'nltk-freq-dist.csv'
+ngrams_csv = fd + 'nltk-ngrams.csv'
+desc_stats_csv = fd + 'nltk-desc-stats.csv'
+
+freq_dist_headers = ['id', 'word', 'frequency']
+ngrams_headers = ['id', 'ngram', 'measure_value', 'measure_type',]
+desc_stats_headers = ['id', 'type', 'value']
+
+# with open(csv_file_name, 'a') as csv_file:
+#     csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#     csv_writer.writerow(headers)
+
+
 
 def load_dataframe(filepath):
     """ Load CSV data as a pandas DataFrame 
     And apply data pre-processing and cleaning """
+    global resource_id  # For CSV normalization
+    resource_id = filepath.split('/')[len(filepath.split('/'))-1] # Using input's file name
+
     if 'youtube' in filepath:
         data = pd.read_csv(filepath, encoding='utf-8')
         comments = data['top_level_comment']
@@ -60,25 +80,83 @@ def freq_dist(data):
     comments = [[w.lower() for w in c if w.lower() not in stopwords and w not in punctuation] for c in comments]
     # comments = [[w for w in c if w not in punctuation] for c in comments]
 
-    # merged_comments = "\n".join([" ".join([w for w in c]) for c in comments])
     merged_comments = []  # Init ignore_var or else list comprehension spams the terminal output
     ignore_var = [[merged_comments.append(w) for w in c] for c in comments]
 
-    text_FD = nltk.FreqDist(merged_comments)
+    text_FD = nltk.FreqDist(merged_comments).most_common(20)
+    pprint(text_FD)
 
-    pprint(text_FD.most_common(20))
+    for i1, i2 in text_FD:
+        with open(freq_dist_csv, 'a') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([resource_id, i1, i2])
 
 
 def collocations(data):
     """ N-grams """
+    comments = [word_tokenize(c) for c in data]
+    comments = [[w.lower() for w in c if w not in punctuation] for c in comments]
+
+    merged_comments = []  # Init ignore_var or else list comprehension spams the terminal output
+    ignore_var = [[merged_comments.append(w) for w in c] for c in comments]
+
+    pprint(nltk.Text(merged_comments).collocations())
+
+    # Simple implementation:
+    # ngrams = nltk.ngrams(merged_comments, 4)
+    # for ng in ngrams:
+    #     pprint(ng)
+
+    # More detailed implementation: http://www.nltk.org/_modules/nltk/metrics/association.html
+    # Possible measures: chi_sq, pmi, likelihood_ratio, jaccard
+    bigram_stats = nltk.collocations.BigramAssocMeasures()
+    bigram_finder = BigramCollocationFinder.from_words(merged_comments)
+    bigram_finder.apply_freq_filter(5)
+    bigrams = bigram_finder.nbest(bigram_stats.pmi, 10)
+    for n1, n2 in bigrams:
+        print n1, n2, bigram_finder.score_ngram(bigram_stats.pmi, n1, n2)
+
+    for n1, n2 in bigrams:
+        score = bigram_finder.score_ngram(bigram_stats.pmi, n1, n2)
+        with open(ngrams_csv, 'a') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([resource_id, n1+' '+n2, score, 'pmi'])
+
+    trigram_stats = nltk.collocations.TrigramAssocMeasures()
+    trigram_finder = TrigramCollocationFinder.from_words(merged_comments, window_size=3)
+    trigram_finder.apply_freq_filter(5)
+    trigrams = trigram_finder.nbest(trigram_stats.pmi, 10)
+    for n1, n2, n3 in trigrams:
+        print n1, n2, n3, trigram_finder.score_ngram(trigram_stats.pmi, n1, n2, n3)
+
+    for n1, n2, n3 in trigrams:
+        score = trigram_finder.score_ngram(trigram_stats.pmi, n1, n2, n3)
+        with open(ngrams_csv, 'a') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([resource_id, n1+' '+n2+' '+n3, score, 'pmi'])
+
+
+def desc_stats(data):
+    """ """
+    lens = []
+    for c in data:
+        lens.append(len(c))
+    ln = pd.DataFrame(lens)
+    desc = ln[0].describe().to_dict()
+    print desc
+
+    for i in desc:
+        with open(desc_stats_csv, 'a') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([resource_id, i, desc[i]])
 
 
 def main(input_data):
     """ Top-level commands """
-    data = load_dataframe(input_data)
-    freq_dist(data)
-
-
+    comments_data = load_dataframe(input_data)
+    freq_dist(comments_data)
+    collocations(comments_data)
+    desc_stats(comments_data)
 
 
 if __name__ == '__main__':
